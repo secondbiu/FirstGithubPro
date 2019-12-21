b96f497cb0757b45d114db8f134b3afcd1d0970f
secondbiu <41848375+secondbiu@users.noreply.github.com>
2019-12-21 11:31:18 +0800

    Add files via upload

---
+++ PageUtil.java
@@ -0,0 +1,48 @@
+package com.common;
+
+import java.util.List;
+
+import com.pojo.User;
+
+public class PageUtil {
+	private int currentPage=1;
+	private int totalPage;
+	private int pageSize=1;
+	private int totalNum=0;
+	private List<User> list = null;
+	public int getCurrentPage() {
+		return currentPage;
+	}
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+	public int getTotalPage() {
+		return totalPage;
+	}
+	public void setTotalPage() {
+		if(totalNum%pageSize==0){
+			totalPage = totalNum/pageSize;
+		}else{
+			totalPage = totalNum/pageSize+1;
+		}
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public int getTotalNum() {
+		return totalNum;
+	}
+	public void setTotalNum(int totalNum) {
+		this.totalNum = totalNum;
+	}
+	public List<User> getList() {
+		return list;
+	}
+	public void setList(List<User> list) {
+		this.list = list;
+	}
+	
+}
